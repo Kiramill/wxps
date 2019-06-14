5dc6f4c9328c8433ee00c91d567a9e86a2807e12
Kiramill <kiramill@yandex.ru>
2019-06-14 05:11:46 +0300

    add LICENSE

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 
 import style from './style.scss';
 
-
-class ClassName {
-    constructosr() {
-        const as = 'asdd';asdsdasd
-        sadsda
-
-    }
-}
-
-new ClassName();
-console.log('Hello sad');
+console.log('Hello');
